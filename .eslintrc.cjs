9e4ad4f5532571884c191252c2c67dd279d7e5fd
lucystwt <lucy-stwt@outlook.com>
2022-11-14 14:41:36 +0800

    feat: 调整 eslint 配置，升级依赖

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,8 @@ module.exports = {
     },
   },
   rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
 
